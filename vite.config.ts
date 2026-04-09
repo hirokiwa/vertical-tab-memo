a8bdedb96ea5412df6cdb883c117c48ddfbcf1e6
hirokiwa <hirokifsys@gmail.com>
2026-04-10 01:57:07 +0900

    Add analytics meta

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
+const googleTagScript = {
+  tag: 'script',
+  attrs: {
+    async: true,
+    src: 'https://www.googletagmanager.com/gtag/js?id=G-TJXMD0CGC7',
+  },
+  injectTo: 'head',
+} as const
+
+const googleTagInlineScript = {
+  tag: 'script',
+  children: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-TJXMD0CGC7');`,
+  injectTo: 'head',
+} as const
+
 export default defineConfig(() => ({
+  plugins: [
+    {
+      name: 'inject-google-tag-on-build',
+      apply: 'build',
+      transformIndexHtml: {
+        order: 'pre',
+        handler: () => [googleTagScript, googleTagInlineScript],
+      },
+    },
+  ],
   build: {
     rollupOptions: {
       input: {
