a2648d5edf7573ff1967a482181dcbca916a0c10
hiroki <89170014+hirokiwa@users.noreply.github.com>
2026-04-14 08:34:35 +0000

    Rename file

--- src/entries/index.ts
+++ src/entries/index.ts
@@ -1,4 +1,4 @@
-import { initializePage } from '../features/memo/bootstrap'
+import { initializePage } from '../features/memo/initialize-page'
 import { readPageConfig } from '../features/memo/page-config'
 import { readMemoStateFromSearch, shouldFocusMemoInputFromSearch } from '../lib/memo'
 
